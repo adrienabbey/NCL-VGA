07f2732d1f0b44311d7aa85f560714ea1c922e94
Adrien Abbey <abbey.12@wright.edu>
2024-04-20 11:45:19 -0400

    progress!

--- VGA_Parser.java
+++ VGA_Parser.java
@@ -1,14 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class VGA_Parser {
     public static void main(String[] args) throws FileNotFoundException {
         String fileContents = loadFile();
         String[] splitFile = fileContents.split("[)],");
-        for (String line : splitFile) {
-            System.out.println(line);
-        }
+        double[][][] vga_matrix;
+        parseVGA(splitFile);
     }
 
     public static String loadFile() throws FileNotFoundException {
@@ -20,5 +20,19 @@ public class VGA_Parser {
         }
         fileScanner.close();
         return fileContents;
+    }
+
+    public static Double[][][] parseVGA(String[] input) {
+        ArrayList<Double> valueList = new ArrayList<Double>();
+        for (String line : input) {
+            String noBrackets = line.replaceAll("[\\[\\](){}]", "");
+            String cleanedLine = noBrackets.replaceAll("\\s", "");
+            // System.out.println(cleanedLine);
+            String[] splitLine = cleanedLine.split(",");
+            for (String value : splitLine) {
+                valueList.add(Double.parseDouble(value));
+            }
+        }
+        // System.out.println(valueList.size());
     }
 }
